12699235c78b8a7927e4c77161298de3effb7c49
Devin Blankenship <failure2fly@gmail.com>
2021-02-18 18:29:17 -0700

    added music files. Added js and added function to play files. Updated layout for HTML and updated CSS to match new CSS Grid

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var myMusic= document.getElementById("music");
-function play() {
-    myMusic.play();
-    console.log("music should be playing")
+function play(){
+    document.getElementById(document.getElementById("pageId").value).play();
 }
+
