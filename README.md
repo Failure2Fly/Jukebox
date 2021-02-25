3009b62d81d7c52f1af254a50e3dc9106f157bd6
Devin Blankenship <failure2fly@gmail.com>
2021-02-24 19:49:09 -0700

    took out Sublime songs and added Incubus. Updated CSS a little more. Mainly for the button effects. Added new functions to js file. Getting Letter, Getting Number, and combing the two and playing the song along with displaying the appropriate info.

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Jukebox
+
+<!-- Completed Functions -->
+1. When Letter is pressed we add className so that button shows as pressed.
+2. When Number is pressed we add className so that button shows as pressed.
+
+
+1. When a Letter is pressed no other letter can be pressed.
+2. When a Number is pressed no other number can be pressed.
+3. Combining the letter and number combo so that we know what song to play.
+4. Play song
+    4a. .button-push class is removed from the pushed buttons, so that no buttons looked pressed any more. (short delay)
+    4b. We display "NOW PLAYING" along with the artist and song title.
+<!-- Functions left to create for MVP -->
+
+5. We make sure no other song can be played while song is playing.
+6. LOOK BACK AND REFACTOR
+
+<!-- Past MVP -->
+1. Add more stylized CSS. Add Neon Aesthetic. Add page turning. Add 3D effect to all of page. Update What's Playing to display more like radio.
+2. Confirm previous functions for playing song are still working accurately.
+3. Look into working with a database for the music/artists/song titles
+4. LOOK BACK AND REFACTOR
+5. Deploy website so others can see and play with it
+
+<!-- Nice to have additions. Way past MVP -->
+1. CSS animation to insert a coin. 
+    1a. Coin needs to be inserted before pushing selection buttons
+2.  Songs can stack on top of one another. 
+    2a. In the display we would scroll and show the up and coming songs after the now playing song. 
+3. LOOK BACK AND REFACTOR
+
--- index.js
+++ index.js
@@ -1,4 +1,94 @@
-function play(){
-    document.getElementById(document.getElementById("pageId").value).play();
+let info = {
+    A1:{artist: 'Blink-182', song: 'All The Small Things'},
+    B1:{artist: 'Blink-182', song: 'The Rock Show'},
+    A2:{artist: 'The Killers', song: 'Mr. Brightside'},
+    B2:{artist: 'The Killers', song: 'Somebody Told Me'},
+    C1:{artist: 'Smashing Pumpkins', song: 'Disarm'},
+    D1:{artist: 'Smashing Pumpkins', song: '1979'},
+    C2:{artist: 'Third Eye Blind', song: 'Semi-Charmed Life'},
+    D2:{artist: 'Third Eye Blind', song: 'Jumper'},
+    E1:{artist: 'Incubus', song: 'Talk Shows On Mute'},
+    F1:{artist: 'Incubus', song: 'Warning'},
+    E2:{artist: 'Red Hot Chili Peppers', song: 'Californication'},
+    F2:{artist: 'Red Hot Chili Peppers', song: 'Dani California'},
+    G1:{artist: 'The Offspring', song: 'Pretty Fly (For A White Guy)'},
+    H1:{artist: 'The Offspring', song: 'Why Don\'t You Get a Job'},
+    G2:{artist: 'Modest Mouse', song: 'Float On'},
+    H2:{artist: 'Modest Mouse', song: 'Dashboard'}
 }
 
+
+// Function grabs id of button (which matches the InnerHTML)
+// Function sets class to the HTML element that matches the id.
+// We then grab the innerHTML so we can pass it later on for the song combo
+function getLetter(Letter){
+    var buttonPressedTop = document.getElementById('FooterMiddleTop')
+    if (buttonPressedTop.classList.contains('button-pressed')){
+        return
+    }
+    else{
+        buttonPressedTop.classList.add("button-pressed")
+
+        var elementter = document.getElementById(Letter)
+        elementter.classList.add("button-push")
+        elementter.classList.add("button")
+        newLetter = elementter.innerHTML
+    }
+}
+
+
+// Function grabs id of button (which matches the InnerHTML)
+// Function sets class to the HTML element that matches the id.
+// We then grab the innerHTML so we can pass it later on for the song combo
+function getNumber(Number){
+    var buttonPressedBottom = document.getElementById('FooterMiddleBottom')
+    if (buttonPressedBottom.classList.contains('button-pressed')){
+        return
+    }
+    else{
+        buttonPressedBottom.classList.add("button-pressed")
+
+        var elementNumber = document.getElementById(Number)
+        elementNumber.classList.add("button-push")
+        elementNumber.classList.add("button")
+        newNumber = elementNumber.innerHTML
+        combineButtons();
+    }
+}
+
+function combineButtons(){
+    var combo = newLetter + newNumber;
+    var newCombo = document.getElementById(combo);
+
+    var artist = info[combo.toString()]
+    var song = info[combo.toString()]
+
+    document.getElementById("NowPlaying").innerHTML = "NOW PLAYING"
+    document.getElementById("Artist").innerHTML = artist.artist
+    document.getElementById("Song").innerHTML = '"' + song.song + '"'
+    newCombo.play()
+    setTimeout(function(){
+        var elemenLetter=document.getElementById(newLetter)
+        elemenLetter.classList.remove("button-push")
+        elemenLetter.classList.remove("button")
+
+        var buttonPressedTop = document.getElementById('FooterMiddleTop')
+        buttonPressedTop.classList.remove('button-pressed')
+
+        var elementNumber = document.getElementById(newNumber)
+        elementNumber.classList.remove("button-push")
+        elementNumber.classList.remove("button")
+
+        var buttonPressedBottom = document.getElementById('FooterMiddleBottom')
+        buttonPressedBottom.classList.remove('button-pressed')
+     }, 2000);
+}
+
+
+function pageLeft(){
+    console.log("Page should turn left unless all the way to left")
+}
+
+function pageRight(){
+    console.log("Page should turn right unless all the way to right")
+}
