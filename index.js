55238c6978abb5d7cd7f440a713538dc64c34992
Devin Blankenship <failure2fly@gmail.com>
2021-05-05 00:30:45 -0700

    Added new songs for I and J. Edited CSS so all songs could fit. Updated song info in html and js.

--- index.js
+++ index.js
@@ -1,124 +1,158 @@
 let info = {
 // Page 1 and 2
-    A1:{artist: 'Blink-182', song: 'All The Small Things', minutes: 2, seconds: 48},
-    B1:{artist: 'Blink-182', song: 'The Rock Show', minutes: 2, seconds: 51},
+    A1:{artist: 'BLINK-182', song: 'All The Small Things', minutes: 2, seconds: 48},
+    B1:{artist: 'BLINK-182', song: 'The Rock Show', minutes: 2, seconds: 51},
 
-    A2:{artist: 'The Killers', song: 'Mr. Brightside', minutes: 3, seconds: 44},
-    B2:{artist: 'The Killers', song: 'Somebody Told Me', minutes: 3, seconds: 17},
+    A2:{artist: 'THE KILLERS', song: 'Mr. Brightside', minutes: 3, seconds: 44},
+    B2:{artist: 'THE KILLERS', song: 'Somebody Told Me', minutes: 3, seconds: 17},
 
-    C1:{artist: 'Smashing Pumpkins', song: 'Disarm', minutes: 3, seconds: 18},
-    D1:{artist: 'Smashing Pumpkins', song: '1979', minutes: 4, seconds: 23},
+    C1:{artist: 'SMASHING PUMPKINS', song: 'Disarm', minutes: 3, seconds: 18},
+    D1:{artist: 'SMASHING PUMPKINS', song: '1979', minutes: 4, seconds: 23},
 
-    C2:{artist: 'Third Eye Blind', song: 'Semi-Charmed Life', minutes: 4, seconds: 28},
-    D2:{artist: 'Third Eye Blind', song: 'Jumper', minutes: 4, seconds: 32},
+    C2:{artist: 'THIRD EYE BLIND', song: 'Semi-Charmed Life', minutes: 4, seconds: 28},
+    D2:{artist: 'THIRD EYE BLIND', song: 'Jumper', minutes: 4, seconds: 32},
 
-    E1:{artist: 'Incubus', song: 'Talk Shows On Mute', minutes: 3, seconds: 51},
-    F1:{artist: 'Incubus', song: 'Warning', minutes: 4, seconds: 40},
+    E1:{artist: 'INCUBUS', song: 'Talk Shows On Mute', minutes: 3, seconds: 51},
+    F1:{artist: 'INCUBUS', song: 'Warning', minutes: 4, seconds: 40},
 
-    E2:{artist: 'Red Hot Chili Peppers', song: 'Californication', minutes: 5, seconds: 29},
-    F2:{artist: 'Red Hot Chili Peppers', song: 'Dani California', minutes: 4, seconds: 42},
+    E2:{artist: 'RED HOT CHILI PEPPERS', song: 'Californication', minutes: 5, seconds: 29},
+    F2:{artist: 'RED HOT CHILI PEPPERS', song: 'Dani California', minutes: 4, seconds: 42},
 
-    G1:{artist: 'The Offspring', song: 'Pretty Fly (For A White Guy)', minutes: 3, seconds: 8},
-    H1:{artist: 'The Offspring', song: 'Why Don\'t You Get A Job', minutes: 2, seconds: 52},
+    G1:{artist: 'THE OFFSPRING', song: 'Pretty Fly (For A White Guy)', minutes: 3, seconds: 8},
+    H1:{artist: 'THE OFFSPRING', song: 'Why Don\'t You Get A Job', minutes: 2, seconds: 52},
 
-    G2:{artist: 'Modest Mouse', song: 'Float On', minutes: 3, seconds: 29},
-    H2:{artist: 'Modest Mouse', song: 'Dashboard', minutes: 4, seconds: 7},
+    G2:{artist: 'MODEST MOUSE', song: 'Float On', minutes: 3, seconds: 29},
+    H2:{artist: 'MODEST MOUSE', song: 'Dashboard', minutes: 4, seconds: 7},
+
+    I1:{artist: 'COHEED AND CAMBRIA', song: 'A Favor House Atlantic', minutes: 3, seconds: 56},
+    J1:{artist: 'COHEED AND CAMBRIA', song: 'Blood Red Summer', minutes: 4, seconds: 7},
+
+    I2:{artist: 'HOT HOT HEAT', song: 'Running Out Of Time', minutes: 2, seconds: 45},
+    J2:{artist: 'HOT HOT HEAT', song: 'Middle Of Nowhere', minutes: 4, seconds: 1},
+
 // Page 3 and 4
-    A3: {artist: 'The Gaslight Anthem', song: '1,000 Years', minutes: 3, seconds: 38},
-    B3: {artist: 'The Gaslight Anthem', song: 'Stay Lucky', minutes: 3, seconds: 8},
+    A3: {artist: 'THE GASLIGHT ANTHEM', song: '1,000 Years', minutes: 3, seconds: 38},
+    B3: {artist: 'THE GASLIGHT ANTHEM', song: 'Stay Lucky', minutes: 3, seconds: 8},
 
     A4: {artist: 'AFI', song: 'The Leaving Song Part Two', minutes: 3, seconds: 31},
     B4: {artist: 'AFI', song: 'Miss Murder', minutes: 3, seconds: 26},
 
-    C3: {artist: 'Motion City Soundtrack', song: 'Everything Is Alright', minutes: 3, seconds: 28},
-    D3: {artist: 'Motion City Soundtrack', song: 'The Future Freaks Me Out', minutes: 3, seconds: 37},
+    C3: {artist: 'MOTION CITY SOUNDTRACK', song: 'Everything Is Alright', minutes: 3, seconds: 28},
+    D3: {artist: 'MOTION CITY SOUNDTRACK', song: 'The Future Freaks Me Out', minutes: 3, seconds: 37},
 
-    C4: {artist: 'The Used', song: 'Take It Away', minutes: 3, seconds: 37},
-    D4: {artist: 'The Used', song: 'Sick Hearts', minutes: 3, seconds: 29},
+    C4: {artist: 'THE USED', song: 'Take It Away', minutes: 3, seconds: 37},
+    D4: {artist: 'THE USED', song: 'Sick Hearts', minutes: 3, seconds: 29},
 
-    E3: {artist: 'Rancid', song: 'Red Hot Moon', minutes: 3, seconds: 36},
-    F3: {artist: 'Rancid', song: 'Civilian Ways', minutes: 4, seconds: 11},
+    E3: {artist: 'RANCID', song: 'Red Hot Moon', minutes: 3, seconds: 36},
+    F3: {artist: 'RANCID', song: 'Civilian Ways', minutes: 4, seconds: 11},
 
-    E4: {artist: 'Unwritten Law', song: 'She Say\'s', minutes: 3, seconds: 59},
-    F4: {artist: 'Unwritten Law', song: 'Seein\' Red', minutes: 3, seconds: 46},
+    E4: {artist: 'UNWRITTEN LAW', song: 'She Say\'s', minutes: 3, seconds: 59},
+    F4: {artist: 'UNWRITTEN LAW', song: 'Seein\' Red', minutes: 3, seconds: 46},
 
-    G3: {artist: 'The Strokes', song: 'Juicebox', minutes: 3, seconds: 17},
-    H3: {artist: 'The Strokes', song: 'I Can\'t Win', minutes: 2, seconds: 34},
+    G3: {artist: 'THE STROKES', song: 'Juicebox', minutes: 3, seconds: 17},
+    H3: {artist: 'THE STROKES', song: 'I Can\'t Win', minutes: 2, seconds: 34},
 
-    G4: {artist: 'Yellowcard', song: 'Ocean Avenue', minutes: 3, seconds: 18},
-    H4: {artist: 'Yellowcard', song: 'Rough Landing, Holly', minutes: 3, seconds: 33},
+    G4: {artist: 'YELLOWCARD', song: 'Ocean Avenue', minutes: 3, seconds: 18},
+    H4: {artist: 'YELLOW CARD', song: 'Rough Landing, Holly', minutes: 3, seconds: 33},
+
+    I3:{artist: 'LESS THAN JAKE', song: 'Look What Happened', minutes: 3, seconds: 41},
+    J3:{artist: 'LESS THAN JAKE', song: 'She\'s Gonna Break Soon', minutes: 3, seconds: 16},
+
+    I4:{artist: 'LOST PROPHETS', song: 'Last Train Home', minutes: 4, seconds: 35},
+    J4:{artist: 'LOST PROPHETS', song: 'A Town Called Hypocrisy', minutes: 3, seconds: 39},
+
 // Page 5 and 6
-    A5: {artist: 'Sum 41', song: 'We\'re All To Blame', minutes: 3, seconds: 38},
-    B5: {artist: 'Sum 41', song: 'Still Waiting', minutes: 2, seconds: 42},
+    A5: {artist: 'SUM 41', song: 'We\'re All To Blame', minutes: 3, seconds: 38},
+    B5: {artist: 'SUM 41', song: 'Still Waiting', minutes: 2, seconds: 42},
 
-    A6: {artist: 'Say Anything', song: 'Wow! I Can Get Sexual Too', minutes: 3, seconds: 11},
-    B6: {artist: 'Say Anything', song: 'A Walk Through Hell', minutes: 3, seconds: 30},
+    A6: {artist: 'SAY ANYTHING', song: 'Wow! I Can Get Sexual Too', minutes: 3, seconds: 11},
+    B6: {artist: 'SAY ANYTHING', song: 'A Walk Through Hell', minutes: 3, seconds: 30},
 
-    C5: {artist: 'Rise Against', song: 'Give It All', minutes: 2, seconds: 50},
-    D5: {artist: 'Rise Against', song: 'Prayer Of The Refugee', minutes: 3, seconds: 21},
+    C5: {artist: 'RISE AGAINST', song: 'Give It All', minutes: 2, seconds: 50},
+    D5: {artist: 'RISE AGAINST', song: 'Prayer Of The Refugee', minutes: 3, seconds: 21},
 
-    C6: {artist: 'Relient K', song: 'In Love With The 80\'s (Pink Tux To The Prom)', minutes: 3, seconds: 8},
-    D6: {artist: 'Relient K', song: 'Must Of Done Something Right', minutes: 3, seconds: 19},
+    C6: {artist: 'RELIENT K', song: 'In Love With The 80\'s (Pink Tux To The Prom)', minutes: 3, seconds: 8},
+    D6: {artist: 'RELIENT K', song: 'Must Of Done Something Right', minutes: 3, seconds: 19},
 
-    E5: {artist: 'New Found Glory', song: 'My Friends Over You', minutes: 3, seconds: 40},
-    F5: {artist: 'New Found Glory', song: 'Ready And Willing', minutes: 3, seconds: 22},
+    E5: {artist: 'NEW FOUND GLORY', song: 'My Friends Over You', minutes: 3, seconds: 40},
+    F5: {artist: 'NEW FOUND GLORY', song: 'Ready And Willing', minutes: 3, seconds: 22},
 
-    E6: {artist: 'Lucero', song: 'I Can Get Us Out Of Here Tonight', minutes: 4, seconds: 14},
-    F6: {artist: 'Lucero', song: 'Sixteen', minutes: 3, seconds: 12},
+    E6: {artist: 'LUCERO', song: 'I Can Get Us Out Of Here Tonight', minutes: 4, seconds: 14},
+    F6: {artist: 'LUCERO', song: 'Sixteen', minutes: 3, seconds: 12},
 
-    G5: {artist: 'Alkaline Trio', song: 'Time To Waste', minutes: 4, seconds: 12},
-    H5: {artist: 'Alkaline Trio', song: 'Love Love Kiss Kiss', minutes: 3, seconds: 25},
+    G5: {artist: 'ALKALINE TRIO', song: 'Time To Waste', minutes: 4, seconds: 12},
+    H5: {artist: 'ALKALINE TRIO', song: 'Love Love Kiss Kiss', minutes: 3, seconds: 25},
 
-    G6: {artist: 'Everclear', song: 'Santa Monica', minutes: 3, seconds: 11},
-    H6: {artist: 'Everclear', song: 'AM Radio', minutes: 3, seconds: 56},
+    G6: {artist: 'EVERCLEAR', song: 'Santa Monica', minutes: 3, seconds: 11},
+    H6: {artist: 'EVERCLEAR', song: 'AM Radio', minutes: 3, seconds: 56},
+    
+    I5:{artist: 'MUSE', song: 'Time Is Running Out', minutes: 3, seconds: 56},
+    J5:{artist: 'MUSE', song: 'Hysteria', minutes: 3, seconds: 50},
+
+    I6:{artist: 'MY CHEMICAL ROMANCE', song: 'I\m Okay (I Promise)', minutes: 3, seconds: 08},
+    J6:{artist: 'MY CHEMICAL ROMANCE', song: 'Welcome To The Black Parade', minutes: 5, seconds: 11},
+
 // Page 7 and 8
-    A7: {artist: 'Bloc Party', song: 'This Mordern Love', minutes: 4, seconds: 25},
-    B7: {artist: 'Bloc Party', song: 'I Still Remember', minutes: 4, seconds: 23},
+    A7: {artist: 'BLOC PARTY', song: 'This Mordern Love', minutes: 4, seconds: 25},
+    B7: {artist: 'BLOC PARTY', song: 'I Still Remember', minutes: 4, seconds: 23},
 
-    A8: {artist: 'Brand New', song: 'Sic Transit Gloria... Glory Fades', minutes: 3, seconds: 6},
-    B8: {artist: 'Brand New', song: 'Jude Law And A Semester Abroad', minutes: 3, seconds: 40},
+    A8: {artist: 'BRAND NEW', song: 'Sic Transit Gloria... Glory Fades', minutes: 3, seconds: 6},
+    B8: {artist: 'BRAND NEW', song: 'Jude Law And A Semester Abroad', minutes: 3, seconds: 40},
 
-    C7: {artist: 'Panic! At The Disco', song: 'I Write Sins Not Tragedies', minutes: 3, seconds: 6},
-    D7: {artist: 'Panic! At The Disco', song: 'Nine In The Afternoon', minutes: 3, seconds: 11},
+    C7: {artist: 'PANIC! AT THE DISCO', song: 'I Write Sins Not Tragedies', minutes: 3, seconds: 6},
+    D7: {artist: 'PANIC! AT THE DISCO', song: 'Nine In The Afternoon', minutes: 3, seconds: 11},
 
-    C8: {artist: 'Something Corporate', song: 'I Woke Up In A Car', minutes: 4, seconds: 13},
-    D8: {artist: 'Something Corporate', song: 'She Paints Me Blue', minutes: 3, seconds: 38},
+    C8: {artist: 'SOMETHING CORPORATE', song: 'I Woke Up In A Car', minutes: 4, seconds: 13},
+    D8: {artist: 'SOMETHING CORPORATE', song: 'She Paints Me Blue', minutes: 3, seconds: 38},
 
-    E7: {artist: 'Taking Back Sunday', song: 'Makedamnsure', minutes: 3, seconds: 32},
-    F7: {artist: 'Taking Back Sunday', song: 'You\'re So Last Summer', minutes: 3, seconds: 3},
+    E7: {artist: 'TAKING BACK SUNDAY', song: 'Makedamnsure', minutes: 3, seconds: 32},
+    F7: {artist: 'TAKING BACK SUNDAY', song: 'You\'re So Last Summer', minutes: 3, seconds: 3},
 
-    E8: {artist: 'The White Stripes', song: 'Seven Nation Army', minutes: 3, seconds: 51},
-    F8: {artist: 'The White Stripes', song: 'Rag And Bone', minutes: 3, seconds: 48},
+    E8: {artist: 'THE WHITE STRIPES', song: 'Seven Nation Army', minutes: 3, seconds: 51},
+    F8: {artist: 'THE WHITE STRIPES', song: 'Rag And Bone', minutes: 3, seconds: 48},
 
-    G7: {artist: 'American Hi-Fi', song: 'The Art Of Losing', minutes: 3, seconds: 22},
-    H7: {artist: 'American Hi-Fi', song: 'The Geek Get The Girls', minutes: 2, seconds: 50},
+    G7: {artist: 'AMERICAN HI-FI', song: 'The Art Of Losing', minutes: 3, seconds: 22},
+    H7: {artist: 'AMERICAN HI-FI', song: 'The Geek Get The Girls', minutes: 2, seconds: 50},
 
-    G8: {artist: 'Anberlin', song: 'A Day Late', minutes: 3, seconds: 25},
-    H8: {artist: 'Anberlin', song: 'Blame Me! Blame Me!', minutes: 3, seconds: 9},
+    G8: {artist: 'ANBERLIN', song: 'A Day Late', minutes: 3, seconds: 25},
+    H8: {artist: 'ANBERLIN', song: 'Blame Me! Blame Me!', minutes: 3, seconds: 9},
+
+    I7:{artist: 'PARAMORE', song: 'Ain\t It Fun', minutes: 4, seconds: 56},
+    J7:{artist: 'PARAMORE', song: 'Still Into You', minutes: 3, seconds: 31},
+
+    I8:{artist: 'QUEENS OF THE STONE AGE', song: '3\'s and 7\'s', minutes: 3, seconds: 34},
+    J8:{artist: 'QUEENS OF THE STONE AGE', song: 'No One Knows', minutes: 4, seconds: 40},
+    
 // Page 9 and 10
-    A9: {artist: 'The Academy Is', song: 'Neighbors', minutes: 3, seconds: 10},
-    B9: {artist: 'The Academy Is', song: 'Slow Down', minutes: 4, seconds: 2},
+    A9: {artist: 'THE ACADEMY IS', song: 'Neighbors', minutes: 3, seconds: 10},
+    B9: {artist: 'THE ACADEMY IS', song: 'Slow Down', minutes: 4, seconds: 2},
 
-    A10: {artist: 'Eve 6', song: 'Inside Out', minutes: 3, seconds: 39},
-    B10: {artist: 'Eve 6', song: 'Jump Back To Me Anytime', minutes: 3, seconds: 25},
+    A10: {artist: 'EVE 6', song: 'Inside Out', minutes: 3, seconds: 39},
+    B10: {artist: 'EVE 6', song: 'Jump Back To Me Anytime', minutes: 3, seconds: 25},
 
-    C9: {artist: 'The Feeling', song: 'Fill My Little World', minutes: 3, seconds: 40},
-    D9: {artist: 'The Feeling', song: 'Set My World On Fire', minutes: 3, seconds: 22},
+    C9: {artist: 'THE FEELING', song: 'Fill My Little World', minutes: 3, seconds: 40},
+    D9: {artist: 'THE FEELING', song: 'Set My World On Fire', minutes: 3, seconds: 22},
 
-    C10: {artist: 'Fountains Of Wayne', song: 'Stacy\'s Mom', minutes: 3, seconds: 18},
-    D10: {artist: 'Fountains Of Wayne', song: 'Sink To The Bottom', minutes: 3, seconds: 12},
+    C10: {artist: 'FOUNTAINS OF WAYNE', song: 'Stacy\'s Mom', minutes: 3, seconds: 18},
+    D10: {artist: 'FOUNTAINS OF WAYNE', song: 'Sink To The Bottom', minutes: 3, seconds: 12},
 
-    E9: {artist: 'Bowling For Soup', song: 'Almost', minutes: 3, seconds: 26},
-    F9: {artist: 'Bowling For Soup', song: 'Hooray For Beer', minutes: 3, seconds: 26},
+    E9: {artist: 'BOWLING FOR SOUP', song: 'Almost', minutes: 3, seconds: 26},
+    F9: {artist: 'BOWLING FOR SOUP', song: 'Hooray For Beer', minutes: 3, seconds: 26},
 
-    E10: {artist: 'The Get Up Kids', song: 'Ten Minutes', minutes: 3, seconds: 13},
-    F10: {artist: 'The Get Up Kids', song: 'Campire Kansas', minutes: 3, seconds: 3},
+    E10: {artist: 'THE GET UP KIDS', song: 'Ten Minutes', minutes: 3, seconds: 13},
+    F10: {artist: 'THE GET UP KIDS', song: 'Campire Kansas', minutes: 3, seconds: 3},
 
-    G9: {artist: 'Good Charlotte', song: 'The Anthem', minutes: 2, seconds: 55},
-    H9: {artist: 'Good Charlotte', song: 'Something Else', minutes: 3, seconds: 19},
+    G9: {artist: 'GOOD CHARLOTTE', song: 'The Anthem', minutes: 2, seconds: 55},
+    H9: {artist: 'GOOD CHARLOTTE', song: 'Something Else', minutes: 3, seconds: 19},
 
-    G10: {artist: 'Green Day', song: 'When I Come Around', minutes: 2, seconds: 58},
-    H10: {artist: 'Green Day', song: 'Good Riddance (Time Of Your Life)', minutes: 2, seconds: 33}
+    G10: {artist: 'GREEN DAY', song: 'When I Come Around', minutes: 2, seconds: 58},
+    H10: {artist: 'GREEN DAY', song: 'Good Riddance (Time Of Your Life)', minutes: 2, seconds: 33},
+
+    I9:{artist: 'SENSES FAIl', song: 'Buried A Lie', minutes: 3, seconds: 15},
+    J9:{artist: 'SENSES FAIl', song: 'Can\'t Be Saved', minutes: 3, seconds: 07},
+
+    I10:{artist: 'YEAH YEAH YEAHS', song: 'Phenomena', minutes: 4, seconds: 10},
+    J10:{artist: 'YEAH YEAH YEAHS', song: 'Maps', minutes: 3, seconds: 42}
 }
 
 
@@ -175,6 +209,9 @@ function combineButtons(){
     document.getElementById("Artist").innerHTML = music.artist
     document.getElementById("Song").innerHTML = '"' + music.song + '"'
     document.getElementById("SongTime").innerHTML = minutes + ':' + seconds;
+    if (seconds < 10){
+        document.getElementById("SongTime").innerHTML = minutes + ':' + '0' + seconds;
+    }
     newCombo.play()
 
     var timer = setInterval(function(){
